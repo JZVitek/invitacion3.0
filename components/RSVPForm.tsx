ed2e8d18fa64ca094d3b1035696d8119a5705fd7
JZVitek <jose.zamora.vitek@gmail.com>
2025-08-26 16:29:47 -0600

    se elimina un log

--- components/RSVPForm.tsx
+++ components/RSVPForm.tsx
@@ -61,8 +61,7 @@ const RSVPForm = () => {
 
     try {
       const response = await axios.post('/api/rsvp', data);
-      console.log('Respuesta del servidor:', response.data);
-
+      
       if (response.data.status === 'success') {
         setSuccessMessage(
           '¡Tu asistencia ha sido confirmada con éxito, recibirás un correo de confirmación con tu pase de entrada!'
